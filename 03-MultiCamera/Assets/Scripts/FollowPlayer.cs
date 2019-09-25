8f04e2ba40458ac110c4fc5581739021b65459bf
Sabina Chen <sabinach@mit.edu>
2019-09-25 01:53:31 -0400

    added multicamera project

---
+++ 03-MultiCamera/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public Transform player;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 1f, -5f);
+    [SerializeField] private bool rotation = true;
+
+    // Update is called once per frame
+    // Camera follows behind player object
+    void LateUpdate()
+    {	
+        transform.position = player.position + offset;
+
+        if(rotation)
+        {
+        	//transform.rotation = Quaternion.Euler(0f, player.eulerAngles.y, 0f);
+        	//transform.LookAt(player.transform);
+        	transform.rotation = player.transform.rotation; //* Quaternion.Euler(0f, player.eulerAngles.y, 0f);
+
+        }
+    }
+}
---
+++ 03-MultiCamera/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private string vertical = "Vertical1";
+    [SerializeField] private string horizontal = "Horizontal1";
+    
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float rotateSpeed = 50f;
+
+    private Vector3 movement;
+    private float rotation;
+
+    void Update()
+    {
+    	movement.z = Input.GetAxis(vertical) * speed * Time.deltaTime;
+    	rotation = Input.GetAxis(horizontal) * rotateSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+    	transform.Translate(movement, Space.Self);
+    	transform.Rotate(0f, rotation, 0f);
+    }
+}
