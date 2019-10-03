87c21b981c20f671d9b64dcfa9b5a1143054182b
Sabina Chen <sabinach@mit.edu>
2019-10-02 20:51:11 -0400

    changed project names, created menu skeleton for cleartheway

---
+++ 03-ClearTheWay/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+    	Debug.Log("Quit Game");
+    	Application.Quit();
+    }
+}
---
+++ 03-ClearTheWay/Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSelect : MonoBehaviour
+{
+    public void LoadOnePlayer()
+    {
+    	//SceneManager.LoadScene("OnePlayer");
+    }
+
+    public void LoadTwoPlayers()
+    {
+    	//SceneManager.LoadScene("TwoPlayers");
+    }
+}
